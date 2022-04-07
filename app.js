ed661b4e57b8881be07cca4f4cbf7092caad2a12
Glitch (getting-started-discord) <none>
2022-04-07 22:39:05 +0000

    💴☂️ Checkpoint ./examples/app.js:365722/12531 ./examples/button.js:365722/4052 ./examples/command.js:365722/4353 ./examples/modal.js:365722/5787 ./examples/selectMenu.js:365722/5425 ./app.js:365722/11879 ./commands.js:365722/8618 ./game.js:365722/5511 ./utils.js:365722/2607 ./package.json:365722/300 ./LICENSE:365722/1068

--- app.js
+++ app.js
@@ -1,60 +1,68 @@
-import express from 'express'
-import axios from 'axios';
-import { InteractionType, InteractionResponseType } from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji, ComponentType, ButtonStyle, DiscordAPI } from './utils.js';
+import express from 'express';
+import { InteractionType, InteractionResponseType, InteractionResponseFlags } from 'discord-interactions';
+import {
+  VerifyDiscordRequest,
+  getRandomEmoji,
+  ComponentType,
+  ButtonStyle,
+  DiscordRequest,
+} from './utils.js';
 import { getShuffledOptions, getResult } from './game.js';
-import { CHALLENGE_COMMAND, TEST_COMMAND, HasGuildCommands } from './commands.js';
+import {
+  CHALLENGE_COMMAND,
+  TEST_COMMAND,
+  HasGuildCommands,
+} from './commands.js';
 
 // Create an express app
 const app = express();
 // Parse request body and verifies incoming requests using discord-interactions package
-app.use(express.json({verify: VerifyDiscordRequest(process.env.PUBLIC_KEY)}));
-
-// Create HTTP client instance with token
-const client = axios.create({
-    headers: {'Authorization': `Bot ${process.env.DISCORD_TOKEN}`}
-});
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
 // Store for in-progress games. In production, you'd want to use a DB
-let activeGames = {};
+const activeGames = {};
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
 app.post('/interactions', async function (req, res) {
-    // Interaction type and data
-    const { type, id, data } = req.body;
+  // Interaction type and data
+  const { type, id, data } = req.body;
 
-    /**
-     * Handle verification requests
-     */
-    if (type === InteractionType.PING) {
-        return res.send({ "type": InteractionResponseType.PONG });
-    }
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
 
-    /**
-     * Handle slash command requests
-     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-     */
-    if (type === InteractionType.APPLICATION_COMMAND){
-        const { name } = data;
-        // "test" guild command
-        if (name === "test") {
-             // Respond to command in channel
-            return res.send({
-                "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                "data": {
-                    // Fetches a random emoji to send from a helper function
-                    "content": "hello world " + getRandomEmoji()
-                }
-            });
-        }
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "test" guild command
+    if (name === 'test') {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: 'hello world ' + getRandomEmoji(),
+        },
+      });
     }
+  }
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+  console.log('Listening on port 3000');
 
-    // Check if guild commands from commands.json are installed (if not, install them)
-    HasGuildCommands(client, process.env.APP_ID, process.env.GUILD_ID, [TEST_COMMAND, CHALLENGE_COMMAND]);
+  // Check if guild commands from commands.json are installed (if not, install them)
+  HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [
+    TEST_COMMAND,
+    CHALLENGE_COMMAND,
+  ]);
 });
