843e4d9add66ab0469ad403ae15198a25d2a88a3
JGK19 <jgkongevold@outlook.com>
2023-02-04 00:51:56 -0300

    tentando criar novo comando

--- app.js
+++ app.js
@@ -92,6 +92,16 @@ app.post("/interactions", async function (req, res) {
         },
       });
     }
+    if (name === "testando") {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: "hello world " + getRandomEmoji(),
+        },
+      });
+    }
   }
 
   /**
