26e917d8f3662ce1b4ca243823d3bcb29e33e388
JGK19 <jgkongevold@outlook.com>
2023-02-05 14:50:04 -0300

    resolvi o problema

--- commands/cifra_dos_tolos.js
+++ commands/cifra_dos_tolos.js
@@ -2,12 +2,12 @@ import { InteractionResponseType } from "discord-interactions";
 
 export function criptografar(req, res) {
 
-  let mensagem = req.body.data.options
+  let mensagem = req.body.data.options[0].value
   console.log('Request no /criptografar:')
   console.log(req.body)
   console.log('vrumvrum')
   console.log(req.body.data.options)
-  console.log(req.body.data.options.value)
+  console.log(req.body.data.options[0].value)
   console.log('vrumvrum')
 
   if ( mensagem == null ) {
