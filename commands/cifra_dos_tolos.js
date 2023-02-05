5443ae6bdb9cd94a768ad4e0e823475f340dd04f
JGK19 <jgkongevold@outlook.com>
2023-02-05 14:52:59 -0300

    limpando os debugs e mensagem de erro

--- commands/cifra_dos_tolos.js
+++ commands/cifra_dos_tolos.js
@@ -3,17 +3,10 @@ import { InteractionResponseType } from "discord-interactions";
 export function criptografar(req, res) {
 
   let mensagem = req.body.data.options[0].value
-  console.log('Request no /criptografar:')
-  console.log(req.body)
-  console.log('vrumvrum')
-  console.log(req.body.data.options)
-  console.log(req.body.data.options[0].value)
-  console.log('vrumvrum')
 
   if ( mensagem == null ) {
-    mensagem = "vrumvrum"
+    mensagem = "manda alguma mensagem ai parceiro"
   }
-  console.log(mensagem)
 
   // Send a message into the channel where command was triggered from
   return res.send({
