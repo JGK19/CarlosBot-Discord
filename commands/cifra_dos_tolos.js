1d809953545d0c90ef9d44d317656fffb102e0b0
JGK19 <jgkongevold@outlook.com>
2023-02-05 14:23:42 -0300

    debug

--- commands/cifra_dos_tolos.js
+++ commands/cifra_dos_tolos.js
@@ -2,6 +2,9 @@ export function criptografar(req, res) {
   console.log('Request no /criptografar:')
   console.log(req.body)
   console.log('vrumvrum')
+  console.log(req.body.data.options)
+  console.log('vrumvrum')
+
 
   // Send a message into the channel where command was triggered from
   return res.send({
