b09f34954fbd18e57d2f6cbbefc5a09bd3efb968
JGK19 <jgkongevold@outlook.com>
2023-02-05 18:04:08 -0300

    /

--- utils.js
+++ utils.js
@@ -9,7 +9,7 @@ export function VerifyDiscordRequest(clientKey) {
     const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
     if (!isValidRequest) {
       res.status(401).send('Bad request signature');
-/      throw new Error('Bad request signature');
+      throw new Error('Bad request signature');
     }
   };
 }
