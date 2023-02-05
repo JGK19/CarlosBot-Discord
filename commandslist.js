9321841ef7e153c2df580a7086d8c5bbe7f415ef
JGK19 <jgkongevold@outlook.com>
2023-02-05 17:55:44 -0300

    s

--- commandslist.js
+++ commandslist.js
@@ -48,7 +48,7 @@ export const commandslist = [
     body: {
       name: "descriptografar",
       description: "Descriptografar uma mensagem usando a Cifra dos tolos",
-      optionS: [
+      options: [
         {
           type: 3,
           name: "frase",
